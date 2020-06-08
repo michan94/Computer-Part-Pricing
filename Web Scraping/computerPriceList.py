97cb92f817c269f18d304842037fa7d5b0b58a4a
Mitchell Chan <michan@Mitchells-MBP.hitronhub.home>
2020-06-08 13:39:31 -0700

    Added data for Memory Express

--- Web Scraping/computerPriceList.py
+++ Web Scraping/computerPriceList.py
@@ -19,7 +19,7 @@ for head in headings:
 	name = head.find('a').text.strip()
 	price = head.find('strong').text.strip() 
 		
-	all_info_regular.append( u' '.join((name, "Price: " + price)).encode('utf-8').strip())
+	all_info_regular.append( u' '.join((name, " - Price: " + price)).encode('utf-8').strip())
 
 pp.pprint("Canada Computers")	
 pp.pprint("SSDs")
@@ -41,14 +41,14 @@ for head in sale_headings:
 	sale_price = head.find('span', class_='text-danger d-block mb-0 pq-hdr-product_price line-height').text.strip()
 	saving = float(reg_price[1:]) - float(sale_price[1:])
 	
-	all_info_sale.append( u' '.join((name, "Regular Price: " + reg_price, "Sale Price: " + sale_price, 'Savings: $' + str(saving))).encode('utf-8').strip())
+	all_info_sale.append( u' '.join((name, " - Regular Price: " + reg_price, "Sale Price: " + sale_price, 'Savings: $' + str(saving))).encode('utf-8').strip())
 
 pp.pprint("On Sale Items")
 pp.pprint(all_info_sale, indent = 4)
 
 
 ###########
-
+#audio_url = head.find('source',{'type':'audio/mpeg'}).get('src')
 
 me_page = 'source_me_ssd.htm'
 soup = BeautifulSoup(open(me_page), "html.parser")
@@ -57,15 +57,44 @@ headings = soup.find_all('div', class_='c-shca-icon-item')
 me_all_info_regular = []
 
 for head in headings:
-	brand = head.find('div', class_='c-shca-icon-item__body-name').get('title')
-	item = head.find('a').text.strip()
+	brand = head.find('div', class_='c-shca-icon-item__body-name').find('img').get('title')
+	#brand = head.find('div', class_='c-shca-icon-item__body').find('img').get('title')
+	item = head.find('div', class_='c-shca-icon-item__body-name').find('a').text.strip()
 	name = str(brand) + " " + str(item)
 	price = head.find('div', class_='c-shca-icon-item__summary-regular').text.strip()
 	
-	me_all_info_regular.append( u' '.join((name, "Price: " + price[4:])).encode('utf-8').strip())
+	me_all_info_regular.append( u' '.join((name, " - Price: " + price[4:])).encode('utf-8').strip())
 	
 pp.pprint("Memory Express")
 pp.pprint("SSDs")
 pp.pprint(me_all_info_regular, indent=4)
 
 #soup.find_all('div', class_='c-shca-icon-item c-shca-icon-item--has-sale')
+
+###########
+
+sale_headings = soup.find_all('div', class_='c-shca-icon-item c-shca-icon-item--has-sale')
+
+me_all_info_sale = []
+
+for head in sale_headings:
+	brand = head.find('div', class_='c-shca-icon-item__body-name').find('img').get('title')
+	item = head.find('div', class_='c-shca-icon-item__body-name').find('a').text.strip()
+	name = str(brand) + " " + str(item)
+	reg_price = head.find('div', class_='c-shca-icon-item__summary-regular').text.strip()
+	sale_price = head.find('div', class_='c-shca-icon-item__summary-list').find('span').text.strip()
+	saving = float(reg_price[6:]) - float(sale_price[1:])
+#	pp.pprint(reg_price[5:])
+#	pp.pprint(sale_price[1:])
+	
+	me_all_info_sale.append( u' '.join((name, " - Regular Price: " + reg_price[5:], "Sale Price: " + sale_price, 'Savings: $' + str(saving))).encode('utf-8').strip())
+#	me_all_info_sale.append( (name, " - Regular Price: " + reg_price[5:], "Sale Price: " + sale_price, 'Savings: $' + str(saving)))
+	
+pp.pprint("On Sale Items")
+pp.pprint(me_all_info_sale, indent=4)
+
+
+
+
+
+
